ce567fc8dc20af2fb8e2daa86073ba4f496eaba8
Josh Savage <yyrael@gmail.con>
2016-07-27 17:07:17 +1200

    Adding Anne lines

---
+++ Assets/Scripts/StartUpManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class StartUpManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Menu");
+
+    }
+}
